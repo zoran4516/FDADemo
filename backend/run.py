905d6cf5720ab33aeeca076d30860b012926652d
Edelweiss35 <45229901+Edelweiss35@users.noreply.github.com>
2019-07-11 19:18:52 -0700

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,4 @@
-"""Entry point for the backend application."""
 
-from pylogging import HandlerType, setup_logger
-
-from flask_app import jwt_server
 
 if __name__ == '__main__':
     setup_logger(log_directory='./logs',
